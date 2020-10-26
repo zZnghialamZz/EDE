fd278b4c0bc4d465a013f3ce8c2027e12ac1043d
Nghia Lam <nghialam12795@gmail.com>
2020-10-26 20:39:27 +0700

    Init project structure

---
+++ scripts/mac/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Author : Nghia Lam
+# Copyright (c) Nghia Lam
+
+/usr/local/bin/cmake --build build
+
+echo "Finish !!"
---
+++ scripts/mac/clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Author : Nghia Lam
+# Copyright (c) Nghia Lam
+
+rm -rf bin
+rm -rf build
+rm -rf compile_command.json
+
+/usr/local/bin/cmake -H. -B build
